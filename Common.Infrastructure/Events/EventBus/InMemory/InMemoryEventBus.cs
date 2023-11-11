a174ea95669ca7eb158439e42cbb6dfe272f0482
Burak Gokoglu <burak.gokoglu@eversana.com>
2023-11-11 13:58:05 -0600

    added logger to inmemoryeventbus

--- Common.Infrastructure/Events/EventBus/InMemory/InMemoryEventBus.cs
+++ Common.Infrastructure/Events/EventBus/InMemory/InMemoryEventBus.cs
@@ -1,14 +1,26 @@
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Common.Infrastructure.Events.EventBus.InMemory;
 
 internal sealed class InMemoryEventBus : IEventBus
 {
     private readonly IMediator _mediator;
+    private readonly ILogger<InMemoryEventBus> _logger;
 
-    public InMemoryEventBus(IMediator mediator) => _mediator = mediator;
+    public InMemoryEventBus(IMediator mediator, ILogger<InMemoryEventBus> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
 
     public async Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default)
-        where TEvent : IIntegrationEvent =>
+        where TEvent : IIntegrationEvent
+    {
+        _logger.LogInformation("Publishing event {Val}", typeof(TEvent));
+        
         await _mediator.Publish(@event, cancellationToken);
+
+        _logger.LogInformation("Published event {Val}", typeof(TEvent));
+    }
 }
