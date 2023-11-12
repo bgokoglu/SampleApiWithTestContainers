741400af4526c4fd0d479225090c60a6b11bae87
Burak Gokoglu <burak.gokoglu@eversana.com>
2023-11-11 21:51:58 -0600

    fix slowqueryinterceptor

--- Common.Infrastructure/Data/SlowQueryInterceptor.cs
+++ Common.Infrastructure/Data/SlowQueryInterceptor.cs
@@ -14,16 +14,17 @@ public class SlowQueryInterceptor : DbCommandInterceptor
         _serviceProvider = serviceProvider;
     }
 
-    private const int SlowQueryThreshold = 100; //milliseconds
+    private const int SlowQueryThreshold = 50; //milliseconds
     
-    public override DbDataReader ReaderExecuted(DbCommand command, CommandExecutedEventData eventData, DbDataReader result)
+    public override ValueTask<DbDataReader> ReaderExecutedAsync(DbCommand command, CommandExecutedEventData eventData, DbDataReader result,
+        CancellationToken cancellationToken = new CancellationToken())
     {
         if (eventData.Duration.TotalMilliseconds > SlowQueryThreshold)
         {
             var logger = _serviceProvider.GetRequiredService<ILogger<SlowQueryInterceptor>>();
-            logger.LogWarning("Slow query {DurationTotalMilliseconds} ms: {CommandText}", eventData.Duration.TotalMilliseconds, command.CommandText);
+            logger.LogWarning("*** Slow Query {DurationTotalMilliseconds} ms: {CommandText}", 
+                eventData.Duration.TotalMilliseconds, command.CommandText);
         }
-
-        return base.ReaderExecuted(command, eventData, result);
+        return base.ReaderExecutedAsync(command, eventData, result, cancellationToken);
     }
 }
